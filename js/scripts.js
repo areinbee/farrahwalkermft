98829218898c2a95dcc9e7d3e9ad4d634e6a7b88
areinbee <are.in.bee@gmail.com>
2025-05-05 15:06:50 -0400

    footer year update, logo banner, simpler speaking page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,6 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const cloneEl = document.querySelector(".logos-slide").cloneNode(true)
+const logos = document.querySelector('.logos').appendChild(cloneEl);
